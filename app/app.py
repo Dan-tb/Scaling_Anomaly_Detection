115a1cfa3e469b52844249b3f33ee3ee9d4db4f5
Dan-tb <izenyidaniel@gmail.com>
2024-10-24 22:08:09 +0100

    some edit commit

--- app/app.py
+++ app/app.py
@@ -9,22 +9,27 @@ from model import ConvVAE
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
+# Initialize the model and load the state dictionary
 model = ConvVAE().to(device)
 
 model_name = "anomaly_model.pth"
 model_path = "C:/Users/USER/Documents/Projects/AI projects/Anomaly_app/Scaling_Anomaly_Detection/data/" + model_name
-state_dict = torch.load(model_path, weights_only=True, map_location="cpu")
-model = model.load_state_dict(state_dict)
+state_dict = torch.load(model_path, map_location=device)  
+model.load_state_dict(state_dict)  
 
+# Set model to evaluation mode
+model.eval()
 
+# Create dataset and dataloader
 image_dir = "C:/Users/USER/Downloads/Garba/Uninfected/"
 dataset = ImageDataset(image_dir=image_dir, transform=transform)
 test_dataloader = torch.utils.data.DataLoader(dataset, batch_size=32, shuffle=True)
 
+# FastAPI initialization
 app = FastAPI(
     title="Anomaly Detection",
-    # description="Finding Anomaly", 
-    version="0.1")
+    version="0.1"
+)
 
 
 class Anomaly_Item(BaseModel):
@@ -42,6 +47,6 @@ async def detect():
     anomalies, errors = detect_anomalies(model, test_dataloader, threshold=0.01)
     
     return [
-        {"image": anomaly["image"], "is_anomaly": anomaly["is_anomaly"]}
+        {"name_id": anomaly["image"], "anomaly_status": anomaly["is_anomaly"]}
         for anomaly in anomalies
     ]
